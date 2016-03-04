139bd6b2767e671b8318a7b634c36f77173823d1
estrategia <andres.tabares@eiso.com.co>
2016-03-04 11:45:45 -0500

    main and login layout

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -58,6 +58,9 @@ class SiteController extends Controller {
     }
 
     public function actionLogin() {
+
+        $this->layout = 'loginLayout' ;
+
         if (!\Yii::$app->user->isGuest) {
             return $this->goHome();
         }
