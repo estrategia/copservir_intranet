635b6dcdb78a3010c1a949cc8abe42887989cb06
estrategia <andres.tabares@eiso.com.co>
2016-03-07 09:41:21 -0500

    views update information user and change password

--- views/site/actualizarDatos.php
+++ views/site/actualizarDatos.php
@@ -10,29 +10,39 @@ use yii\captcha\Captcha;
 $this->title = 'Actualizar Datos';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
+<div class="col-md-8">
+  <div class="page-title">
+      <h4>  Actualiza tu información personal</h4>
+  </div>
 
+  <div class="grid simple">
+    <div class="grid-title no-border">
 
-    <?php
-    $form = ActiveForm::begin([
-                'id' => 'login-form',
-                'options' => ['class' => 'form-horizontal'],
-                'fieldConfig' => [
-                    'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-                    'labelOptions' => ['class' => 'col-lg-1 control-label'],
-                ],
-    ]);
-    ?>
-
-    <?= $form->field($model, 'email') ?>
-    <?= $form->field($model, 'celular') ?>
-    <?= $form->field($model, 'residencia') ?>
-    <div class="s"
-    <div class="form-group">
-        <div class="col-lg-offset-1 col-lg-11">
-            <?= Html::submitButton('Guardar', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-        </div>
     </div>
-    <?php ActiveForm::end(); ?>
+    <div class="grid-body no-border">
 
+      <?php
+      $form = ActiveForm::begin([
+                  'id' => 'login-form',
+                  'options' => ['class' => 'form-horizontal'],
+                  'fieldConfig' => [
+                      'template' => "{label}\n<div class=\"col-md-6 controls\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
+                      'labelOptions' => ['class' => 'col-lg-1 control-label'],
+                  ],
+      ]);
+      ?>
+
+      <?= $form->field($model, 'email') ?>
+      <?= $form->field($model, 'celular') ?>
+      <?= $form->field($model, 'residencia') ?>
+
+      <div class="form-group">
+          <div class="col-lg-offset-1 col-lg-11">
+              <?= Html::submitButton('Actualizar', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+          </div>
+      </div>
+      <?php ActiveForm::end(); ?>
+
+    </div>
+  </div>
 </div>
--- views/site/cambiarClave.php
+++ views/site/cambiarClave.php
@@ -10,7 +10,7 @@ use yii\captcha\Captcha;
 $this->title = 'Recuperación contraseña';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
+<!--<div class="site-login">
 
     <p>Cambiar clave:</p>
 
@@ -19,7 +19,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 'id' => 'login-form',
                 'options' => ['class' => 'form-horizontal', 'enableClientValidation' => true],
                 'fieldConfig' => [
-                    'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
+                    'template' => "{label}\n<div class=\"col-md-6 controls\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
                     'labelOptions' => ['class' => 'col-lg-1 control-label'],
                 ],
     ]);
@@ -38,4 +38,41 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?php ActiveForm::end(); ?>
 
+</div>-->
+
+<div class="col-md-8">
+  <div class="page-title">
+      <h4> Actualiza tu clave de acceso</h4>
+  </div>
+
+  <div class="grid simple">
+    <div class="grid-title no-border">
+
+    </div>
+    <div class="grid-body no-border">
+
+      <?php
+      $form = ActiveForm::begin([
+                  'id' => 'login-form',
+                  'options' => ['class' => 'form-horizontal', 'enableClientValidation' => true],
+                  'fieldConfig' => [
+                      'template' => "{label}\n<div class=\"col-md-6 controls\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
+                      'labelOptions' => ['class' => 'col-lg-1 control-label'],
+                  ],
+      ]);
+      ?>
+
+      <?= $form->field($model, 'password')->passwordInput() ?>
+      <?= $form->field($model, 'password2')->passwordInput()->label('Confirmar contraseña') ?>
+      <?= $form->field($model, 'captcha')->widget(Captcha::className())->label("") ?>
+
+      <div class="form-group">
+          <div class="col-lg-offset-1 col-lg-11">
+              <?= Html::submitButton('Actualizar', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+          </div>
+      </div>
+      <?php ActiveForm::end(); ?>
+
+    </div>
+  </div>
 </div>
