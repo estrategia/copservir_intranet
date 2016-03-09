218d411d8259d57dfedaa257c63810f2f254aa41
jjaragon <juan.aragon@eiso.com.co>
2016-03-09 17:41:45 -0500

    Se confirma cambios en el menú y la jerarquización de items con el menú.pendiente, mostrar todas las opciones del menú.

---
+++ modules/intranet/models/Opcion.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace app\modules\intranet\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "m_Opcion".
+ *
+ * @property string $idOpcion
+ * @property string $nombrePermiso
+ * @property string $url
+ * @property integer $estado
+ * @property string $idMenu
+ */
+class Opcion extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'm_Opcion';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['nombrePermiso', 'url', 'estado', 'idMenu'], 'required'],
+            [['estado', 'idMenu'], 'integer'],
+            [['nombrePermiso'], 'string', 'max' => 45],
+            [['url'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'idOpcion' => 'Id Opcion',
+            'nombrePermiso' => 'Nombre Permiso',
+            'url' => 'Url',
+            'estado' => 'Estado',
+            'idMenu' => 'Id Menu',
+        ];
+    }
+}
