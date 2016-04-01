0f97b6c226101337880ec9a296a002d6c027aa1a
msanchez-eiso <>
2016-04-01 00:12:45 -0500

    Notificaciones mecanismo httpush

--- modules/intranet/controllers/DefaultController.php
+++ modules/intranet/controllers/DefaultController.php
@@ -6,41 +6,48 @@ use yii\web\Controller;
 use yii\web\Session;
 use app\modules\intranet\models\EventosCalendario;
 
-class DefaultController extends Controller
-{
-    public function actionIndex()
-    {
+class DefaultController extends Controller {
+
+    public function actionIndex() {
         echo \Yii::$app->params['grupo']['*'];
         //return $this->render('index');
     }
-    
-    public function actionSession()
-    {
+
+    public function actionSession() {
         //var_dump(\Yii::$app->user->identity);
         //return $this->render('index');
-        
         //$session = new Session;
         //$session->open();
         $session = \Yii::$app->session;
-        
+
         //$value1 = $session['name1'];  // get session variable 'name1'
         //$value2 = $session['name2'];  // get session variable 'name2'
         //foreach ($session as $name => $value) // traverse all session variables
-        var_dump($session['prueba']); 
+        var_dump($session['prueba']);
     }
-    
-    public function actionEventos(){
+
+    public function actionEventos() {
         //$models = EventosCalendario::consultarEventos();
         $inicio = '2016-03-01';
         $fin = '2016-03-31';
-        
-        
+
+
         $listEventos = EventosCalendario::consultarEventos($inicio, $fin, true);
-        
+
         \yii\helpers\VarDumper::dump($listEventos, 5, true);
     }
-    
-    public function actionFecha(){
+
+    public function actionFecha() {
         echo date('Y-m-d H:i:s');
     }
+
+    public function actionNotif() {
+        $listNotificaciones = \app\modules\intranet\models\Notificaciones::consultarNotificaciones();
+
+        foreach ($listNotificaciones as $objNotificacion) {
+            $objNotificacion->consultarTiempo();
+            
+        }
+    }
+
 }
--- modules/intranet/views/layouts/main_1.php
+++ modules/intranet/views/layouts/main_1.php
@@ -115,7 +115,7 @@ $opciones->opcionesUsuario(Yii::$app->user->identity->numeroDocumento);
                     <!-- BEGIN CHAT TOGGLER -->
                     <div class="pull-right">
                         <div class="chat-toggler">
-                            <a href="#" class="dropdown-toggle" id="my-task-list" data-placement="bottom"  data-content='' data-toggle="dropdown" data-original-title="Notificaciones">
+                            <a href="#" class="dropdown-toggle" id="my-task-list" data-placement="bottom"  data-content='' data-toggle="dropdown" data-original-title="Notifications">
                                 <div class="user-details">
                                     <div class="username">
                                         <span class="badge badge-important">3</span>
@@ -125,7 +125,59 @@ $opciones->opcionesUsuario(Yii::$app->user->identity->numeroDocumento);
                                 <div class="iconset top-down-arrow"></div>
                             </a>
                             <div id="notification-list" style="display:none">
-                                
+                                <div style="width:300px">
+                                    <div class="notification-messages info">
+                                        <div class="user-profile">
+                                            <img src= <?= "" . $srcPictureUser ?> alt="" data-src="" data-src-retina="" width="35" height="35">
+                                        </div>
+                                        <div class="message-wrapper">
+                                            <div class="heading">
+                                                David Nester - Commented on your wall
+                                            </div>
+                                            <div class="description">
+                                                Meeting postponed to tomorrow
+                                            </div>
+                                            <div class="date pull-left">
+                                                A min ago
+                                            </div>
+                                        </div>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                    <div class="notification-messages danger">
+                                        <div class="iconholder">
+                                            <i class="icon-warning-sign"></i>
+                                        </div>
+                                        <div class="message-wrapper">
+                                            <div class="heading">
+                                                Server load limited
+                                            </div>
+                                            <div class="description">
+                                                Database server has reached its daily capicity
+                                            </div>
+                                            <div class="date pull-left">
+                                                2 mins ago
+                                            </div>
+                                        </div>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                    <div class="notification-messages success">
+                                        <div class="user-profile">
+                                            <img src=""  alt="" data-src="" data-src-retina="" width="35" height="35">
+                                        </div>
+                                        <div class="message-wrapper">
+                                            <div class="heading">
+                                                You haveve got 150 messages
+                                            </div>
+                                            <div class="description">
+                                                150 newly unread messages in your inbox
+                                            </div>
+                                            <div class="date pull-left">
+                                                An hour ago
+                                            </div>
+                                        </div>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                </div>
                             </div>
                             <div class="profile-pic">
                                 <img src=<?= "" . $srcPictureUser ?> alt="" data-src="" data-src-retina="" width="35" height="35" />
