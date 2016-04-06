9d429bf53e1c275db3c90a77af3075ec433dd32c
estrategia <andres.tabares@eiso.com.co>
2016-04-06 16:06:50 -0500

    borrar menu chat y ajuste icons menu 2

--- modules/intranet/views/layouts/main_1.php
+++ modules/intranet/views/layouts/main_1.php
@@ -285,14 +285,14 @@ $opciones->opcionesUsuario(Yii::$app->user->identity->numeroDocumento);
                 </div>
             </div>
             <a href="#" class="scrollup">Scroll</a>
-            <div class="footer-widget">
+            <!--<div class="footer-widget">
                 <div class="progress transparent progress-small no-radius no-margin">
                     <div class="progress-bar progress-bar-success animate-progress-bar" data-percentage="79%" style="width: 79%;"></div>
                 </div>
                 <div class="pull-right">
                     <div class="details-status"> <span class="animate-number" data-value="86" data-animation-duration="560">86</span>% </div>
                     <a href="lockscreen.html"><i class="fa fa-power-off"></i></a></div>
-            </div>
+            </div>-->
             <!-- END SIDEBAR -->
             <!-- BEGIN PAGE CONTAINER-->
             <div class="page-content">
