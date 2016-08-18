3d8626ce2fa72ebc7a2764b9e7892fd6c4c41a98
miguel <miguel.bernal@eiso.com.co>
2016-08-18 09:51:26 -0500

    ajustes varios

--- modules/intranet/views/contenido/publicaciones.php
+++ modules/intranet/views/contenido/publicaciones.php
@@ -1,11 +1,14 @@
 <?php
 use yii\widgets\ListView;
 
-if ($todasArea) {
+if ($todasArea === 'area') {
   $this->title = 'Publicaciones de mi área';
-}else{
+}else if($todasArea === 'todas'){
   $this->title = 'Todas las publicaciones';
+}else if($todasArea === 'mispublicaciones'){
+  $this->title = 'Mis publicaciones';
 }
+
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
