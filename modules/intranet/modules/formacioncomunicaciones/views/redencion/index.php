9efcc86433674c6fb37f13fabce6de1fe209fb5f
felipePatino <felipe.patino@eiso.com.co>
2017-05-03 11:13:51 -0500

    Estilos premios formacion comunicaciones

--- modules/intranet/modules/formacioncomunicaciones/views/redencion/index.php
+++ modules/intranet/modules/formacioncomunicaciones/views/redencion/index.php
@@ -3,23 +3,21 @@
 use yii\helpers\Html;
 use yii\helpers\Url;
 ?>
-<h1>Categorías de premios</h1>
+<h3>Categorías de premios</h3>
 <div class="row">
 
 <?= Html::img(Yii::getAlias('@web').'/img/formacioncomunicaciones/assets/banner.png', ['class' => 'img-responsive']) ?>
 <div class="col-md-10">
   <?php foreach ($categorias as $categoria): ?>
     <div class="col-sm-4 col-md-4 item">
-    <div class="categoria-item">
-      <h4 class="nombre-categoria"><?= $categoria->nombreCategoria ?></h4>
-      <?= Html::img(Yii::getAlias('@web').'/img/formacioncomunicaciones/categorias/'. $categoria->rutaIcono, ['class' => 'img-responsive ']) ?>  
-       <div class="sub-cat">
-          <a href="<?= Url::to(['redencion/subcategorias', 'idCategoria' => $categoria->idCategoria]) ?>">
-            <?= Html::img(Yii::getAlias('@web').'/img/formacioncomunicaciones/assets/mas.png', ['class' => 'more']) ?>
-          </a>
-       </div> 
-    </div>
-    
+    <a href="<?= Url::to(['redencion/subcategorias', 'idCategoria' => $categoria->idCategoria]) ?>">
+      <div class="categoria-item">
+        <h4 class="nombre-categoria"><?= $categoria->nombreCategoria ?></h4>
+        <?= Html::img(Yii::getAlias('@web').'/img/formacioncomunicaciones/categorias/'. $categoria->rutaIcono, ['class' => 'img-responsive ']) ?>     
+
+        <?= Html::img(Yii::getAlias('@web').'/img/formacioncomunicaciones/assets/mas.png', ['class' => 'more']) ?>  
+      </div>
+    </a>    
     </div>
   <?php endforeach ?>
 </div>
--- modules/intranet/modules/formacioncomunicaciones/views/redencion/subcategorias.php
+++ modules/intranet/modules/formacioncomunicaciones/views/redencion/subcategorias.php
@@ -2,18 +2,36 @@
 /* @var $this yii\web\View */
 use yii\helpers\Html;
 use yii\helpers\Url;
-
 ?>
+<h3>Categorías de premios</h3>
+<div class="row">
+  <?= Html::img(Yii::getAlias('@web').'/img/formacioncomunicaciones/assets/banner.png', ['class' => 'img-responsive']) ?>
 
-<div class="categorias-premios-redencion-container">
+  <div class="col-md-10">
     <?php foreach ($categoria->categoriasPremios as $categoria): ?>
-      <div class="categoria-premios-redencion-item">
-        <a href="<?= Url::to(['redencion/premios-categoria', 'idCategoria' => $categoria->idCategoria]) ?>">
-          <?= Html::img(Yii::getAlias('@web').'/img/formacioncomunicaciones/categorias/'. $categoria->rutaIcono, ['class' => 'img-responsive thumbnail']) ?>
-          <h4>
-            <?= $categoria->nombreCategoria ?>
-          </h4>
-        </a>
+      <div class="col-sm-4 col-md-4 item">
+        <div class="sub-categoria-item">
+         
+            <div class="puntos">
+              <?= Html::img(Yii::getAlias('@web').'/img/formacioncomunicaciones/assets/puntos.png', ['class' => 'img-responsive']) ?> 
+              <span class="quantity">15.000 <br> puntos</span>              
+            </div>
+
+            <?= Html::img(Yii::getAlias('@web').'/img/formacioncomunicaciones/categorias/'. $categoria->rutaIcono, ['class' => 'img-responsive']) ?>
+
+            <h4 class="nombre-sub-categoria"><?= $categoria->nombreCategoria ?></h4>
+          
+          <a href="/">
+          <?= Html::img(Yii::getAlias('@web').'/img/formacioncomunicaciones/assets/redimir.png', ['class' => 'redimir']) ?>
+          </a> 
+        </div>
+        
       </div>
     <?php endforeach ?>
+
+  </div>
+
+
+
+
   </div>
