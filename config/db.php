06d8b7cc96c54ed718d463bacb54f936f74e39c1
msanchez-eiso <eiso@192.168.0.30>
2017-08-18 10:01:07 -0500

    merge msanchex

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=192.168.1.22;dbname=intranet',
-    //'dsn' => 'mysql:host=localhost;dbname=intranet',
+    //'dsn' => 'mysql:host=192.168.1.29;dbname=intranet',
+    'dsn' => 'mysql:host=localhost;dbname=intranet',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
